2b1fe2f51ee6ab6d295cb385ab541ef675c58306
mikhail-barg <mikhail.barg@gmail.com>
2019-06-12 11:38:57 +0300

    building for nuget

--- RusstressNet/Properties/AssemblyInfo.cs
+++ RusstressNet/Properties/AssemblyInfo.cs
@@ -2,35 +2,32 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RusstressNet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Automatic accent (stress) prediction for Russian language")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mikhail Barg")]
 [assembly: AssemblyProduct("RusstressNet")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("809f55b5-14e1-4eb2-8611-df0c69526d04")]
+[assembly: Guid("d0dd5c0d-51f0-4f4b-8347-194ee8f36ff2")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RusstressNet/nuget_pack.bat
@@ -0,0 +1,2 @@
+nuget pack -properties Configuration=Release
+nuget pack -Symbols -SymbolPackageFormat snupkg -properties Configuration=Release
